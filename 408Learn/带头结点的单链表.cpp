08e28d6ece2f3db9f73962ba7534e50ce658ec48
JOKER <1169937491@qq.com>
2026-03-05 20:58:47 +0800

    补充单链表操作

--- 408Learn/带头结点的单链表.cpp
+++ 408Learn/带头结点的单链表.cpp
@@ -54,3 +54,72 @@ bool InsertPriorNode(LNode* p, int e) {
 	p->data = e;
 	return true;
 }
+//按序号插入节点
+bool InsertNextNode(LinkList& L,int i, int e) {
+	if (i < 1) {
+		return false;
+	}
+	int j = 0;//记录当前节点位序
+	LNode* p = L;
+	while (p != NULL && j < i - 1) {
+		p = p->next;
+		j++;
+	}
+	if (p == NULL) {
+		return false;
+	}
+	LNode* s = (LNode*)malloc(sizeof(LNode*));
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return true;
+}
+//按序号查找节点
+LNode* GetElem(LinkList &L, int i) {
+	if (i < 1) {
+		return NULL;
+	}
+	LNode* p = L;
+	while (i > 0&&p!=NULL) {
+		p = p->next;
+		i--;
+	}
+	return p;
+}
+//按值查找
+LNode* LocateElem(LinkList& L, int e) {
+	LNode* p = L;
+	while (p != NULL && p->data != e) {
+		p = p->next;
+	}
+	return p;
+}
+//删除节点已知位序
+bool ListDelete(LinkList& L, int i,int &e) {
+	if (i < 0) {
+		return false;
+	}
+	int j = 0;
+	LNode* p = L;
+	while (p != NULL && j < i - 1) {
+		p = p->next;
+		j++;
+	}
+	if (p == NULL) {
+		return false;
+	}
+	e = p->next->data;
+	//不要写成p->next = p->next ->next;节点实际没被删除,内存不会被释放
+	LNode* s = p->next;
+	p->next = s->next;
+	free(s);//清除的是s节点指向的内存空间
+	return true;
+}
+//删除已知节点(该方法删除时会有问题，采用从头判断没问题逻辑如位序)
+bool ListDelete(LinkList& L,LNode* q ,int i, int& e) {
+	e=q->data;
+	LNode* p = q->next;//当q为链表链尾元素时p为NULL,p->next不存在
+	p->data = q->data;
+	q->next = p->next;
+	free(p);
+}
